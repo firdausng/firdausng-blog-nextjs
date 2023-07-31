142b2711c4edd5b636b0ef4da08fddc50acab908
firdaus <mail4frenz@gmail.com>
2023-07-31 15:48:51 +0800

    fix error handling on id not found

--- src/app/blog/[postId]/page.tsx
+++ src/app/blog/[postId]/page.tsx
@@ -25,7 +25,7 @@ export default async function BlogDetail(props: ProjectPageProps){
         return <NotFoundSection />
     }
 
-    if(!data.draft){
+    if(data.draft){
         return <NotFoundSection />
     }
 
